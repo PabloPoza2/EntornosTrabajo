9436e718d812de243495a991ddcd76bc7783b357
diego <diego@LAPTOP-010Q2TEM>
2024-01-04 16:24:00 +0100

    calculo area y perimetro de una circunferencia

---
+++ Trabajo/src/Trabajo/circunferencia.java
@@ -0,0 +1,28 @@
+package Trabajo;
+import java.util.Scanner;
+
+public class circunferencia {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa el radio de la circunferencia: ");
+        double radio = scanner.nextDouble();
+
+        double area = calcularArea(radio);
+        double perimetro = calcularPerimetro(radio);
+
+        System.out.println("Área de la circunferencia: " + area);
+        System.out.println("Perímetro de la circunferencia: " + perimetro);
+
+        scanner.close();
+    }
+
+    public static double calcularArea(double radio) {
+        return Math.PI * Math.pow(radio, 2);
+    }
+
+    public static double calcularPerimetro(double radio) {
+        return 2 * Math.PI * radio;
+    }
+}
