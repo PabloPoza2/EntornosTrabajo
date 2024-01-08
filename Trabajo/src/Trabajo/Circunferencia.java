37256afd36082f0b38bba5b4c0aa4b0f8de5d52e
diego <diego@1DAM-09>
2024-01-08 08:48:37 +0100

    circunferencia

--- Trabajo/src/Trabajo/Circunferencia.java
+++ Trabajo/src/Trabajo/Circunferencia.java
@@ -1,5 +1,29 @@
 package Trabajo;
 
+import java.util.Scanner;
+
 public class Circunferencia {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa el radio de la circunferencia: ");
+        double radio = scanner.nextDouble();
+
+        double area = calcularArea(radio);
+        double perimetro = calcularPerimetro(radio);
+
+        System.out.println("Área de la circunferencia: " + area);
+        System.out.println("Perímetro de la circunferencia: " + perimetro);
+
+        scanner.close();
+    }
+
+    public static double calcularArea(double radio) {
+        return Math.PI * Math.pow(radio, 2);
+    }
+
+    public static double calcularPerimetro(double radio) {
+        return 2 * Math.PI * radio;
+    }
 }
